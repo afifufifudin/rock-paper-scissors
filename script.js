565455b1e7c6a47aa88028d5656805f24a8650fa
afifudin <27afifudin@gmail.com>
2024-05-20 11:11:46 +0700

    add functionality to the button

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
 // console.log("Hello world");
 let playerScore = 0
 let computerScore = 0
+let roundWinner = ''
+let player;
+let computer;
 
-// let getComputerChoice = () =>{
-//     const choice = ['rock', 'paper', 'scissors'];
-//     const random = Math.floor(Math.random() * choice.length);
-//     return choice[random];
-// }
-
-// let getPlayerChoice = ()=>{
-//     const choice = prompt("What do you wanna use?");
-//     return choice.toLowerCase();
-// }
+// const rock = document.querySelector("#rock")
+// const paper = document.querySelector("#paper")
+// const scissors = document.querySelector("#scissors")
+const buttons = document.querySelectorAll(".button")
+const player_choice = document.querySelector("#player-choice") 
+const computer_choice = document.querySelector("#computer-choice")
+const player_score = document.querySelector("#player-score")
+const computer_score = document.querySelector("#computer-score")
+const result_commentary = document.querySelector(".result")
+const modal = document.querySelector(".modal")
+const overlay = document.querySelector(".overlay") 
+const play_again = document.querySelector(".play-again")
 
 let playRound = (player, computer) => {
     let result;
-    player = getPlayerChoice();
-    computer = getComputerChoice();
+    // player = getPlayerChoice();
+    // computer = getComputerChoice();
     if(player === computer){
-        result = (`both choose ${computer}, No one wins`);
+        result = (`both choose ${computer}, No one wins this round`);
+        result_commentary.setAttribute("style", "color: None;")
+        player_choice.setAttribute("style", "background-color: #eef1f5")
+        computer_choice.setAttribute("style", "background-color: #eef1f5")
     } else if (
         // computer wins
         (player === 'rock' && computer === 'paper') ||
         (player === 'paper' && computer === 'scissors') ||
         (player === 'scissors' && computer === 'rock')
     ){
-        result = (`${computer} beat ${player}, Computer wins`);
+        result = (`${computer} beat ${player}, Computer wins this round`);
         computerScore ++;
+        result_commentary.setAttribute("style", "color: red;")
+        player_choice.setAttribute("style", "background-color: #ff998a")
+        computer_choice.setAttribute("style", "background-color: #b0ffb1")
+
     } else if(
         // player wins
         (computer === 'rock' && player === 'paper') ||
         (computer === 'paper' && player === 'scissors') ||
         (computer === 'scissors' && player === 'rock')
     ){
-        result = (`${player} beat ${computer}, Player wins`);
+        result = (`${player} beat ${computer}, You wins this round`);
         playerScore ++;
+        result_commentary.setAttribute("style", "color: green;")
+        player_choice.setAttribute("style", "background-color: #b0ffb1")
+        computer_choice.setAttribute("style", "background-color: #ff998a")
+        
     }
-    return console.log(result)
+    return result
 }
 
 let game = () =>{
@@ -44,7 +60,6 @@ let game = () =>{
     while(true){
         total_score = playerScore + computerScore;
         playRound();
-        console.log(`PLAYER ${playerScore} | ${computerScore} COMPUTER`);
         if(total_score === 5){
             console.log('Game over')
             console.log(`PLAYER ${playerScore} | ${computerScore} COMPUTER`);
@@ -59,13 +74,62 @@ let game = () =>{
             break;
         }
     }
-    // while((total_score !== 5) || (playerScore || computerScore <= 3)){
-    //     console.log(`PLAYER ${playerScore} | ${computerScore} COMPUTER`);
-    //     playRound();
-    //     total_score = playerScore + computerScore;
-    // }
 }
 
-game();
 
-console.log(`PLAYER ${playerScore} | ${computerScore} COMPUTER`);
+let getComputerChoice = () =>{
+    const choice = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * choice.length);
+    return choice[random];
+}
+
+let update_score = () =>{
+    player_score.textContent = `${playerScore}`
+    computer_score.textContent = `${computerScore}`
+}
+
+let commentary = (result) => {
+    result_commentary.textContent = `${result}`
+}
+
+let selection = (button) =>{
+    player_choice.textContent = button.innerText;
+}
+
+let getComputerEmoji = (choice) =>{
+    switch(choice){
+        case 'rock':
+            return '✊'
+        case 'paper':
+            return '✋'
+        case 'scissors':
+            return '✌️'
+    }
+}
+
+let openModal = () =>{
+    modal.classList.add('active')
+    overlay.classList.add('active')
+    play_again.classList.add('active')
+}
+
+let checkScore = (player, computer) =>{
+    if(playerScore == 5 || computerScore == 5){
+        if(playerScore == 5){
+            modal.textContent = "You Win"
+        }else{
+            modal.textContent = "You Lose"
+        }
+        openModal();
+    }
+}
+
+buttons.forEach((button)=>{button.addEventListener('click', ()=>{
+    player = button.id
+    computer = getComputerChoice()
+    commentary(playRound(player, computer))
+    update_score()
+    selection(button)
+    computer_choice.textContent = getComputerEmoji(computer)
+    checkScore(playerScore, computerScore)
+})})
